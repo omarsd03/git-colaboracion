443c2d93cf9cfbc8bab508a3b9110a0917933e6c
Mariana Arce <Mariana.Arce@ext.saint-gobain.com>
2021-06-09 09:42:49 -0500

    Nuevo Commit V1.2.1

--- js/app.js
+++ js/app.js
@@ -1,2 +1,5 @@
 console.log('Hola mundo!');
 console.log('Este es mi pueba de commit');
+// Agregando comentarios
+// y más comentarios 
+/* para que funcione la prueba de colaboración con versiones en Git jeejejejejej*/
